1890cbea87e2334d77f106fb607128f96c5e29e8
Vinh <qdo086@uottawa.ca>
2017-12-05 14:52:34 -0500

    2i, might be wrong will test later

--- ParisMetro.java
+++ ParisMetro.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.HashMap;
 import net.datastructures.Edge;
 import net.datastructures.Graph;
 import net.datastructures.GraphAlgorithms;
@@ -13,21 +15,19 @@ import net.datastructures.HeapPriorityQueue;
 import net.datastructures.AdjacencyMapGraph;
 
 public class ParisMetro{
-    private Graph<Integer,Integer> metroGraph;
-    private ArrayList<String> allStations;
+    public AdjacencyMapGraph<Integer,Integer> metroGraph;
+    public ArrayList<String> allStations;
+    public HashMap<Integer, Boolean> sameStations;
 
-    public void test(){
-        metroGraph = new AdjacencyMapGraph<Integer,Integer>(true);
-    }
-
-    public ParisMetro(String fileName){
+    public ParisMetro(){
         metroGraph = new AdjacencyMapGraph<Integer,Integer>(true);
         allStations = new ArrayList<String>();
+        sameStations = new HashMap<Integer, Boolean>();
     }
 
-    public static Graph readMetro(String fileName, ArrayList<String> stations) throws IOException{
+    public static AdjacencyMapGraph readMetro(String fileName, ArrayList<String> stations) throws IOException{
         AdjacencyMapGraph<Integer,Integer> wGraph = new AdjacencyMapGraph<Integer,Integer>(true);
-        Hashtable<Integer, Vertex> vertices = new Hashtable<Integer, Vertex>();
+        HashMap<Integer, Vertex> hVertices = new HashMap<>();
         BufferedReader bufferedRead = new BufferedReader(new FileReader(fileName));
         String line;
         String[] data;
@@ -38,15 +38,16 @@ public class ParisMetro{
             data = line.split(" ", 2);
             int stationID = Integer.parseInt(data[0]);
             stations.add( stationID, data[1] );
-            vertices.put( stationID, wGraph.insertVertex( stationID ) );
+            Vertex<Integer> newStation = wGraph.insertVertex( stationID );
+            hVertices.put( stationID, newStation );
             line = bufferedRead.readLine().trim();
         }
 
         line = bufferedRead.readLine();
         while( line != null ) {
             data = line.split(" ", 3);
-            Vertex<Integer> vertexFrom = vertices.get( Integer.parseInt(data[0]) );
-            Vertex<Integer> vertexTo = vertices.get( Integer.parseInt(data[1]) );
+            Vertex<Integer> vertexFrom = hVertices.get( Integer.parseInt(data[0]) );
+            Vertex<Integer> vertexTo = hVertices.get( Integer.parseInt(data[1]) );
             int weight = Integer.parseInt(data[2]);
             wGraph.insertEdge( vertexFrom, vertexTo, weight);
             line = bufferedRead.readLine();
@@ -56,17 +57,95 @@ public class ParisMetro{
         return wGraph;
     }
 
+    public void sameLine( int stationID ) throws Exception{
+        Vertex<Integer> startStation = getVertex( stationID );
+        stationsOnSameLine( startStation );
+    }
+
+    private void stationsOnSameLine( Vertex<Integer> station ) {
+
+        if( station == null || sameStations.containsKey( station.getElement() ) ){
+            return;
+        }
+
+        sameStations.put( station.getElement() , true);
+
+        Iterator<Edge<Integer>> it = metroGraph.outgoingEdges( station ).iterator();
+        Vertex<Integer> visitStation;
+        Edge<Integer> line;
+
+        while( it.hasNext() ) {
+            line = it.next();
+            visitStation = metroGraph.opposite( station, line );
+            
+            if( line.getElement() != -1 ){                
+                stationsOnSameLine( visitStation );
+            }
+        }
+
+        return;
+    }
+
+/*
+
+  private void sameLine(Graph<String,String> graph, Vertex<String> v ) {
+    
+        if(v == null || visited.containsKey(v.getElement())) return;
+    
+        visited.put(v.getElement(), true);
+        startVisit(v);
+    
+        Iterator<Edge<String>> edges = graph.outgoingEdges(v).iterator();
+        Vertex<String> opposite;
+    
+        while(edges.hasNext()) {
+            opposite = graph.opposite(v, edges.next());
+            DFS(graph, opposite);
+        }
+    
+        finishVisit(v);
+    
+        return;
+}*/
+
+    // From Lab net library
+	protected Vertex<Integer> getVertex(int vert) throws Exception {
+		// Go through vertex list to find vertex -- why is this not a map
+		for (Vertex<Integer> vs : metroGraph.vertices()) {
+			if (vs.getElement().equals(vert)) {
+				return vs;
+			}
+        }
+		throw new Exception("Vertex not in graph: " + vert);
+	}
+
+
     public static void main(String[] args){
-        ParisMetro metro = new ParisMetro("metro.txt");
-        //metro.test();
+        ParisMetro metro = new ParisMetro();
         try{
-            readMetro("metro.txt", metro.allStations);
+            metro.metroGraph = ParisMetro.readMetro("metro.txt", metro.allStations);
+            metro.sameLine(39);
+            for(int i = 0 ; i < 375; i++){
+                if( metro.sameStations.containsKey(i)){
+                    System.out.println(metro.allStations.get(i));
+                }
+            }
+            
         }
         catch(IOException e){
-            System.out.println("IOEXCEPTION");
+            System.out.println("IOException e");
         }
         catch(Exception e){
             System.out.println("EXCEPTION");
         }
+        
+        //ArrayList<Vertex> sameStations = metro.stationsOnSameLine( 200 );
+     
+/*
+        Iterator it = metro.allStations.iterator();
+        while(it.hasNext()){
+            System.out.println( (String) it.next() );
+        }*/
+        
     }
 }
